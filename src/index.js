4e947d45607bf64355ccad729ecbb625be331963
twicks95 <teguh.wck95@gmail.com>
2021-05-26 12:27:22 +0700

    New commit

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const helmet = require('helmet')
 const compression = require('compression')
 const bodyParser = require('body-parser')
 const routerNavigation = require('./routes')
+const socket = require('socket.io')
 
 const app = express()
 const port = process.env.DB_PORT
@@ -24,6 +25,53 @@ app.use(bodyParser.json())
 app.use('/backend3/api/v1', routerNavigation)
 app.use('/backend3/api', express.static('src/uploads'))
 
-app.listen(port, () => {
+// Socket.io configuration=================
+const server = require('http').createServer(app)
+const io = socket(server, {
+  cors: {
+    origin: '*'
+  },
+  path: '/backend3/socket.io'
+})
+io.on('connection', (socket) => {
+  console.log('Socket.io Connect !')
+  // global Message = pesan yang dikirim ke semua client
+  socket.on('globalMessage', (data) => {
+    console.log(data)
+    io.emit('chatMessage', data)
+  })
+  // private Message = pesan yang dikirim ke client saja
+  socket.on('privateMessage', (data) => {
+    console.log(data)
+    socket.emit('chatMessage', data)
+  })
+  // private Message = pesan yang dikirim ke semua client kecuali pengirim
+  socket.on('broadcastMessage', (data) => {
+    console.log(data)
+    socket.broadcast.emit('chatMessage', data)
+  })
+  // ==========================================
+  socket.on('joinRoom', (data) => {
+    console.log(data)
+    if (data.oldRoom) {
+      socket.leave(data.oldRoom)
+    }
+    socket.join(data.room)
+    socket.broadcast.to(data.room).emit('chatMessage', {
+      username: '[BOT]',
+      message: `${data.username} join this Chat`
+    })
+  })
+  socket.on('roomMessage', (data) => {
+    io.to(data.room).emit('chatMessage', data)
+  })
+})
+
+server.listen(port, () => {
   console.log(`Express app is listen on port ${port} !`)
 })
+// =======================================
+
+// app.listen(port, () => {
+//   console.log(`Express app is listen on port ${port} !`)
+// })
